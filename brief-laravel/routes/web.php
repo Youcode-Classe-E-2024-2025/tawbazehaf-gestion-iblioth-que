da2914943ee8952be83ec43203a0d67fc9d65223
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2025-02-18 10:46:28 +0100

    adding the routes

--- brief-laravel/routes/web.php
+++ brief-laravel/routes/web.php
@@ -1,4 +1,7 @@
 <?php
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BorrowingController;
 
 use Illuminate\Support\Facades\Route;
 /**
@@ -15,15 +18,15 @@ use Illuminate\Support\Facades\Route;
  */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::post('/login', function () {
-    return view('login');
-});
-Route::get('/register', function () {
-    return view('register');
-});
-Route::get('/index', function () {
-    return view('index');
-});
+ Route::get('/', function () {
+    return view('index'); 
+
+// User routes
+Route::resource('users', UserController::class);
+Route::post('logout', [UserController::class, 'logout'])->name('logout');
+
+// Book routes
+Route::resource('books', BookController::class);
+
+// Borrowing routes
+Route::resource('borrowings', BorrowingController::class);});
